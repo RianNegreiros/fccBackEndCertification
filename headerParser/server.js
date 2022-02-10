dd1adb726fc5ad910ab3878492e506026803fabb
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-02-10 14:54:07 -0300

    return ip address

--- headerParser/server.js
+++ headerParser/server.js
@@ -25,6 +25,9 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami", (req, res) => {
+  res.json({ "ipaddress": req.ip })
+})
 
 
 // listen for requests :)
