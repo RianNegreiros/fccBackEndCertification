bc7ea866cba8141f336e1eecb72b0a254a918d86
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-02-13 22:57:13 -0300

    route to create user

--- exerciseTracker/server.js
+++ exerciseTracker/server.js
@@ -1,17 +1,67 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const shortId = require("shortid")
 require('dotenv').config()
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+//Connect to database
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
+//Schemas
+const exerciseSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  count: Number,
+  duration: { type: Number, required: true },
+  date: Date,
+});
+const Exercise = new mongoose.model('exercise', exerciseSchema);
 
+const userSchema = new mongoose.Schema({
+  _id: { type: String, required: true, default: shortId.generate },
+  username: { type: String, required: true },
+  count: { type: Number, default: 0 },
+  log: [exerciseSchema]
+});
 
+const User = mongoose.model("User", userSchema);
+
+// Post route to create user
+app.post("/api/users", (req, res) => {
+  // Try to find
+  User.findOne({ username: req.body.username }, (err, foundUser) => {
+    // Check is findOne return error and output to console
+    if (err) {
+      console.log('FindOne() error')
+    }
+    // Check if found user by given username
+    if (foundUser) {
+      res.send("Username already in use");
+    } else {
+      // Craete and save a new user
+      const newUser = new User({
+        username: req.body.username
+      });
+      newUser.save();
+      res.json({
+        username: req.body.username,
+        _id: newUser._id
+      });
+    }
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
