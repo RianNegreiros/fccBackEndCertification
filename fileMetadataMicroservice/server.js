e44622b6266ffa05528f43420ddf56cea2a0a5c5
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2022-02-16 20:06:23 -0300

    all tests passed

--- fileMetadataMicroservice/server.js
+++ fileMetadataMicroservice/server.js
@@ -1,18 +1,44 @@
 var express = require('express');
 var cors = require('cors');
+const multer  = require('multer');
+const upload = multer({  dest: 'public/files' })
+const mongoose = require('mongoose');
 require('dotenv').config()
 
 var app = express();
 
 app.use(cors());
+app.use(express.json())
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true
+});
 
+const fileSchema = new mongoose.Schema({
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  name: {
+    type: String,
+    required: [true, 'Uploaded file must have a name'],
+  },
+});
 
+const File = mongoose.model('File', fileSchema)
+
+app.post('/api/fileanalyse', upload.single('upfile') , (req, res) => {
+  res.send({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
